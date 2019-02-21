d6cbc1c2c3cd4e0fe90b30cbd011fae0051a8ec7
sagar.saindre <sagar.saindre@wgblindia.com>
2019-02-21 20:46:41 +0530

    new react app created

---
+++ hello-world/src/shagi.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Shaggi extends Component {
+
+  render() {
+    return(
+      <div>
+        Shaggi
+      </div>
+    );
+  }
+}
+
+export default Shaggi;
